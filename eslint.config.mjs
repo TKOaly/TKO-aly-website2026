65088197e45c0f91baef22574990711f0f0f923f
Boriss Jerjomkin <b.b.j.1998@gmail.com>
2026-02-03 14:59:30 +0200

    Issue #14: Add ESLint configuration for CSS files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,17 @@
 import { defineConfig, globalIgnores } from "eslint/config"
 import nextVitals from "eslint-config-next/core-web-vitals"
 import nextTs from "eslint-config-next/typescript"
+import css from "@eslint/css"
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  {
+    files: ["**/*.css"],
+    language: "css/css",
+    plugins: { css },
+    extends: ["css/recommended"],
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
