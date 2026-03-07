811a345852f3abf782418574c04c2a496b697ec2
Boriss Jerjomkin <b.b.j.1998@gmail.com>
2026-03-07 20:42:37 +0200

    chore: Refactor CSS styles and update page structure for improved layout and readability

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -27,57 +27,60 @@ export default function Index() {
       </div>
       <div id="index-body">
         <div className="diagonal-div">
-          <h1>Tervetuloa!</h1>
-          <p>
-            TKO-äly ry on Helsingin yliopiston
-            <a href="http://www.cs.helsinki.fi/" rel="noopener noreferrer">
-              {" "}
-              tietojenkäsittelytieteen ja datatieteen{" "}
-              <Image
-                src="/external-url-10.png"
-                alt="External link"
-                width={10}
-                height={10}
-              />
-            </a>
-            opiskelijoiden ainejärjestö, joka ajaa opiskelijoiden etua
-            opintoasioissa ja järjestää moninaista vapaa-ajan toimintaa.
-          </p>
-          <p>
-            Meidät tavoittaa Kumpulan kampuksen Exactum-rakennuksen
-            opiskelijahuoneesta DK115
-            <a
-              href="http://www.cs.helsinki.fi/contact/exactum-kartat.html#pohja"
-              rel="noopener noreferrer"
-            >
-              {" "}
-              Gurula{" "}
-              <Image
-                src="/external-url-10.png"
-                alt="External link"
-                width={10}
-                height={10}
-              />
-            </a>
-            , jossa myös tarjoamme jäsenistöllemme omakustannehintaan kahvia,
-            limpparia ja naposteltavaa.
-          </p>
-          <p>
-            Toiminnastamme saat lisää tietoa näiden sivujen lisäksi seuraamalla
-            <a
-              href="https://www.tko-aly.fi/yhdistys/tiedotus"
-              rel="noopener noreferrer"
-            >
-              tiedotustamme{" "}
-              <Image
-                src="/external-url-10.png"
-                alt="External link"
-                width={10}
-                height={10}
-              />
-            </a>
-            .
-          </p>
+          <div>
+            <h1>Tervetuloa!</h1>
+            <p>
+              TKO-äly ry on Helsingin yliopiston
+              <a href="http://www.cs.helsinki.fi/" rel="noopener noreferrer">
+                {" "}
+                tietojenkäsittelytieteen ja datatieteen{" "}
+                <Image
+                  src="/external-url-10.png"
+                  alt="External link"
+                  width={10}
+                  height={10}
+                />
+              </a>
+              opiskelijoiden ainejärjestö, joka ajaa opiskelijoiden etua
+              opintoasioissa ja järjestää moninaista vapaa-ajan toimintaa.
+            </p>
+            <p>
+              Meidät tavoittaa Kumpulan kampuksen Exactum-rakennuksen
+              opiskelijahuoneesta DK115
+              <a
+                href="http://www.cs.helsinki.fi/contact/exactum-kartat.html#pohja"
+                rel="noopener noreferrer"
+              >
+                {" "}
+                Gurula{" "}
+                <Image
+                  src="/external-url-10.png"
+                  alt="External link"
+                  width={10}
+                  height={10}
+                />
+              </a>
+              , jossa myös tarjoamme jäsenistöllemme omakustannehintaan kahvia,
+              limpparia ja naposteltavaa.
+            </p>
+            <p>
+              Toiminnastamme saat lisää tietoa näiden sivujen lisäksi{" "}
+              seuraamalla
+              <a
+                href="https://www.tko-aly.fi/yhdistys/tiedotus"
+                rel="noopener noreferrer"
+              >
+                tiedotustamme{" "}
+                <Image
+                  src="/external-url-10.png"
+                  alt="External link"
+                  width={10}
+                  height={10}
+                />
+              </a>
+              .
+            </p>
+          </div>
         </div>
       </div>
     </div>
